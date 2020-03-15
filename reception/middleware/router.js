c2208d146db49598e7a9caeb0bad4725f15ef083
maliang <maliang1@dgg.net>
2020-03-15 20:36:55 +0800

    页面权限控制

--- reception/middleware/router.js
+++ reception/middleware/router.js
@@ -6,8 +6,6 @@ export default function({ route, req, res, redirect }) {
     const userToken = Cookies.get("userToken")
       ? Cookies.get("userToken")
       : null;
-    alert(userToken);
-    // 需要进行权限判断的页面开头
     if (!userToken) {
       redirect("/");
     }
