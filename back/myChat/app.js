cf7531a3ab77c8ef70a20dc6436c1bb8a682ffde
maliang <maliang1@dgg.net>
2020-03-19 23:59:30 +0800

    修改app.js

--- back/myChat/app.js
+++ back/myChat/app.js
@@ -11,18 +11,6 @@ var usersCenter = require("./routes/user-center");
 var app = express();
 
 
-// 自定义跨域中间件(后台解决跨域))
-var allowCors = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-};
-app.use(allowCors); //使用跨域中间件
-
-
-
 require("./db/index"); //引入mongodb数据库配置
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -45,12 +33,22 @@ app.use(
     })
 );
 
+// 配置跨域请求中间件(服务端允许跨域请求)
+var allowCors = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin); // 设置允许来自哪里的跨域请求访问（req.headers.origin为当前访问来源的域名与端口）
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"); // 设置允许接收的请求类型
+    res.header("Access-Control-Allow-Headers", "Content-Type,request-origin"); // 设置请求头中允许携带的参数
+    res.header("Access-Control-Allow-Credentials", "true"); // 允许客户端携带证书式访问。保持跨域请求中的Cookie。注意：此处设true时，Access-Control-Allow-Origin的值不能为 '*'
+    res.header("Access-control-max-age", 1000); // 设置请求通过预检后多少时间内不再检验，减少预请求发送次数
+    next();
+};
+app.use(allowCors); // 使用跨域中间件
+
 app.all("*", function(req, res, next) {
     req.session._garbage = Date(); /** 每次请求更新session有效时间 */
     req.session.touch();
     /** 请求拦截 */
-    console.log(req.get("request-origin"))
-    // if (req.get("request-origin") && req.get("request-origin") === "WAP") {
+    if (req.get("request-origin") && req.get("request-origin") === "WAP") {
         if (req.originalUrl == "/user-center/register") {
             next();
             return;
@@ -64,13 +62,13 @@ app.all("*", function(req, res, next) {
             return;
         }
         next();
-    // } else {
-    //     res.send({
-    //         code: 414,
-    //         msg: "缺少请求来源request-origin",
-    //         data: null
-    //     });
-    // }
+    } else {
+        res.send({
+            code: 414,
+            msg: "缺少请求来源request-origin",
+            data: null
+        });
+    }
 });
 
 /** 用户中心 */
