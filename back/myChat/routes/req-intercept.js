9bc608c556a53751008c1c7c790bf4cf43e650da
maliang <maliang1@dgg.net>
2020-07-06 19:12:36 +0800

    fix：用户上传头像

--- back/myChat/routes/req-intercept.js
+++ back/myChat/routes/req-intercept.js
@@ -3,12 +3,13 @@
  * */
 var session = require("express-session");
 module.exports.interceptConfig = function (req, res, next) {
+    console.log(777888);
     /** 配置跨域 */
     res.header("Access-Control-Allow-Origin", req.headers.origin); // 设置允许来自哪里的跨域请求访问（req.headers.origin为当前访问来源的域名与端口）
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"); // 设置允许接收的请求类型
     res.header(
         "Access-Control-Allow-Headers",
-        "Content-Type,request-origin,user-token"
+        "Content-Type,request-origin,user-token,_id"
     ); // 设置请求头中允许携带的参数
     res.header("Access-Control-Allow-Credentials", "true"); // 允许客户端携带证书式访问。保持跨域请求中的Cookie。注意：此处设true时，Access-Control-Allow-Origin的值不能为 '*'
     res.header("Access-control-max-age", 1000); // 设置请求通过预检后多少时间内不再检验，减少预请求发送次数
@@ -19,7 +20,6 @@ module.exports.interceptConfig = function (req, res, next) {
 
     /** 请求拦截 */
     if (req.get("request-origin") && req.get("request-origin") === "WAP") {
-        console.log(123, req);
         if (
             req.originalUrl != "/user-center/login" &&
             req.originalUrl != "/user-center/register" &&
--- back/myChat/routes/user-center.js
+++ back/myChat/routes/user-center.js
@@ -2,13 +2,18 @@
  * 用户中心
  *  */
 var express = require("express");
+let formidable = require("formidable");
+const formatTime = require("silly-datetime");
+const fs = require("fs");
+const mongoose = require("mongoose");
+
 var router = express.Router();
 const {
     serviceRegister,
     serviceLogin,
     serviceSearchUser,
     serviceSetUserRemarks,
-    serviceLoginStateModify
+    serviceLoginStateModify,
 } = require("../service/user-center"); //引入服务层的方法
 
 /**
@@ -44,6 +49,102 @@ router.get("/searchUser", async function (req, res, next) {
 // 设置用户备注名和标签
 router.get("/setUserRemarks", async function (req, res, next) {
     res.send(await serviceSetUserRemarks(req));
+});
+
+// 上传图片
+router.post("/upload", async function (req, res, next) {
+    let AVATAR_UPLOAD_FOLDER = "/avatar";
+    //创建上传表单
+    var form = new formidable.IncomingForm();
+    //设置编码格式
+    form.encoding = "utf-8";
+    //设置上传目录
+    form.uploadDir = "./public" + AVATAR_UPLOAD_FOLDER;
+    //保留后缀
+    form.keepExtensions = true;
+    //文件大小
+    form.maxFieldsSize = 2 * 1024 * 1024;
+    form.parse(req, function (err, fields, files) {
+        let filesFile = files.file;
+        if (err) {
+            return res.json({
+                code: 500,
+                msg: "内部服务器错误",
+                data: "",
+            });
+        }
+        // 限制文件大小 单位默认字节 这里限制大小为2m
+        if (filesFile.size > form.maxFieldsSize) {
+            fs.unlink(filesFile.path);
+            return res.json({
+                code: 414,
+                msg: "图片大小不能超过2M",
+                data: "",
+            });
+        }
+        console.log(form.headers._id);
+        //后缀名
+        var extName = "";
+        switch (filesFile.type) {
+            case "image/pjpeg":
+                extName = "jpg";
+                break;
+            case "image/jpeg":
+                extName = "jpg";
+                break;
+            case "image/png":
+                extName = "png";
+                break;
+            case "image/x-png":
+                extName = "png";
+                break;
+            case "image/gif":
+                extName = "gif";
+                break;
+        }
+        if (!extName) {
+            return res.json({
+                code: 414,
+                msg: "不支持上传该图片格式",
+                result: "",
+            });
+        }
+        //使用第三方模块silly-datetime
+        var t = formatTime.format(new Date(), "YYYYMMDDHHmmss");
+        //生成随机数
+        var ran = parseInt(Math.random() * 8999 + 10000);
+        // 生成新图片名称
+        var avatarName = t + "_" + ran + "." + extName;
+        // 新图片路径
+        var newPath = form.uploadDir + "/" + avatarName;
+        // 更改名字和路径
+        fs.rename(filesFile.path, newPath, async function (err) {
+            if (err) {
+                return res.json({
+                    code: 414,
+                    message: "图片上传失败",
+                });
+            } else {
+                let img =
+                    "http://172.16.75.192:3002" +
+                    AVATAR_UPLOAD_FOLDER +
+                    "/" +
+                    avatarName;
+                let userInfo = await mongoose
+                    .model("userCenter")
+                    .findByIdAndUpdate(
+                        form.headers._id,
+                        { headImg: img },
+                        { new: true }
+                    );
+                return res.json({
+                    code: 200,
+                    msg: "上传成功",
+                    data: userInfo,
+                });
+            }
+        });
+    });
 });
 
 module.exports = router;
